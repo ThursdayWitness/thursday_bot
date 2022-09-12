4329d7e3bad84d0ff6452b232843bc1b23049e14
ThursdayWitness <lirkir1256@gmail.com>
2022-09-13 02:18:30 +0500

    successfully changes certain words to stickers

---
+++ sticker_set.py
@@ -0,0 +1,20 @@
+stickers = {
+    'csb': 'CAACAgIAAxkBAAED-otjH1RHVIOAY5yYd964CZ62YEialwACyAMAAuB5Ugf5pW2ZGcswwB4E', # CoolStoryBob
+    'fuck': 'CAACAgEAAxkBAAED-oxjH1TFRPcEmdHX9tYryHEgZ_1EdgACuAIAAmv_ag-ry1qRfwZrYx4E', # Annoyed Link
+    'wtf': 'CAACAgIAAxkBAAED-o5jH1UbfNJummkwk5I5tZmDv0qr9gACCAEAAqb6-iDY6ISR4F1YCx4E', # WTF Dante
+    'KEKW': 'CAACAgIAAxkBAAED-o9jH1VQOyTXQUI9ZvEpAVx5o_1eWwACEg0AAmnM4Usk2QhpG7xijB4E',
+    'tfdespair': 'CAACAgIAAxkBAAED-pBjH1Vjdfmm2TyzZHE8zkc9FHlZVQACTBcAAp8t4Et20VjBp0x_0x4E',
+    'noooo': 'CAACAgIAAxkBAAED-pFjH1V1vkqPKGBjcBzvNQjHhpxFvgACjBgAAkisYEgZdb0fMLAa4x4E', # crying emoji
+    'hooray': 'CAACAgIAAxkBAAED-pJjH1WH5gz7j0nbOQuqK2OpuJVvbwACCQwAAg8wwEpENQJd-JigcB4E',
+    'zubki': 'CAACAgQAAxkBAAED-pNjH1WZbDSOps5NSIrnfh8PlLEgJQACpgsAAvc9cFKyhVfvZ0i6sR4E',
+    'FUCK': 'CAACAgEAAxkBAAED-pRjH1Wvqb72Jwjrz9eEsq5uvRbehwACxgIAAmv_ag_90w1CQCZkPh4E', # Furious Link
+    'rly': 'CAACAgEAAxkBAAED-pVjH1XCYKxCAZk5qO0_63niyg_lqAAC0AEAAmv_ag8Vt-tSTsfXTR4E', # Tired Link
+    'SRSLY': 'CAACAgEAAxkBAAED-pZjH1XQFzmBJRehJbPexyy91I7P_AACxgIAAmv_ag_90w1CQCZkPh4E', # Angry Link
+    'fuck2': 'CAACAgEAAxkBAAED-pdjH1Xh48xOT9hLo6h_sO-aRtelDwACxAIAAmv_ag-Sp0SVsomcdh4E', # Annoyed Link 2
+    'fuck3': 'CAACAgEAAxkBAAED-phjH1YGdCA3Wmtyb5oGkU8cNaTEEQACygIAAmv_ag_XEBrKLd-Z7h4E', # Annoyed Link 3
+    'cool': 'CAACAgEAAxkBAAED-pljH1Ya_BV_wpe7eInYG0GYNDPeggACVgEAAmv_ag9wKV8AAQ2dy78eBA', # Happy Link
+    'eblo': 'CAACAgIAAxkBAAED-ppjH1YxvwH2QZuu9EEe2gI2ZAmjlAACAgADxIsdFfIV2-W3v3I9HgQ',
+    'WTF': 'CAACAgEAAxkBAAED-ptjH1ZORFpoeDCxl_aWCXhV3JwTOwACrgIAAhCtywSrY5IfUHKEvx4E', # Scared Vergil
+    'oklol': 'CAACAgEAAxkBAAED-pxjH1ZatoteVf-QtN_DDm6TmvuRowACrwIAAhCtywQC7Y9CZ8ACfR4E', # Buscemi Vergil
+    'NOOOO': 'CAACAgEAAxkBAAED-p1jH1Z7seoSUOTUlcCOa-MC3VnbeAACSQIAAhCtywTQN0Aodt1hox4E', # Shouting Nero
+}
